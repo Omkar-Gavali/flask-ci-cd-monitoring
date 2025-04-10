59408e329e3a43ddc3d9916d90798fcfa017583c
omkar <Omkar.Gavali@Student.HTW-Berlin.de>
2025-04-11 00:22:15 +0200

    test_app created

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 from flask import Flask
-import time
-from prometheus_client import start_http_server, Summary
-
 app = Flask(__name__)
 
-# Prometheus metric
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-
 @app.route('/')
-@REQUEST_TIME.time()
-def hello():
-    time.sleep(0.5)  # Simulated delay
-    return "Hello from Omkar's Flask App with CI/CD + Monitoring!"
+def home():
+    return "CI/CD Pipeline without Docker is working!"
 
 if __name__ == '__main__':
-    start_http_server(8000)  # Prometheus on port 8000
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
